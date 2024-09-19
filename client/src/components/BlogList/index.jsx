9a8dc2029439298dff1a5eb26a3a3eb065f420f3
StevenDB <xieyijie98@gmail.com>
2024-09-19 19:35:31 +1000

    refactor: blog list as reusable component

---
+++ client/src/components/BlogList/index.jsx
@@ -0,0 +1,74 @@
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
+import { Divider, Flex, List, Space, Typography } from 'antd';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import route from '../../route';
+import { getDateString } from '../../utils/dateUtil';
+import styles from './BlogList.module.css';
+
+const { Paragraph } = Typography;
+
+function ListItem({ data }) {
+  const navigate = useNavigate();
+
+  let previewText = data.previewText;
+  if (previewText && (previewText.slice(-1) !== '.')) {
+    previewText += ' ...';
+  }
+  const createdAt = getDateString(data.createdAt);
+
+  return (
+    <List.Item>
+      <List.Item.Meta
+        title={<a className={styles.listItemTitle}>{data.title || 'Untitled'}</a>}
+      />
+      {previewText && <Paragraph className={styles.listItemContent}>{previewText}</Paragraph>}
+      <Flex justify="space-between" className={styles.listItemFooter}>
+        <Space size="middle">
+          <ListItemFooter text={createdAt} />
+          {/*<ListFooterItem icon={LikeOutlined} text={data.likes} />*/}
+          {/*<ListFooterItem icon={MessageOutlined} text={data.comments} />*/}
+        </Space>
+        <Space split={<Divider type="vertical" />} size={4}>
+          <ListItemFooter
+            className={styles.clickable}
+            icon={EditOutlined}
+            text="Edit"
+            size={6}
+            onClick={() => {
+              navigate(`${route.edit}/${data._id}`);
+            }}
+          />
+          <ListItemFooter className={styles.clickable} icon={DeleteOutlined} text="Delete" size={6} />
+        </Space>
+      </Flex>
+    </List.Item>
+  )
+}
+
+function ListItemFooter({ icon, text, size, className, onClick }) {
+  return (
+    <Space size={size} className={className} onClick={onClick}>
+      {icon && React.createElement(icon)}
+      {text}
+    </Space>
+  );
+}
+
+function BlogList({ data, loading }) {
+  return (
+    <List
+      itemLayout="vertical"
+      size="large"
+      pagination={{
+        position: 'bottom',
+        align: 'center'
+      }}
+      loading={loading}
+      dataSource={loading ? [] : data}
+      renderItem={(data) => <ListItem data={data} />}
+    />
+  );
+}
+
+export default BlogList;
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,79 +1,14 @@
-import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
-import { Divider, Flex, List, Space, Typography } from 'antd';
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import { getAllBlogs } from '../../api/services/blogService';
+import BlogList from '../../components/BlogList';
 import Error from '../../components/Error';
 import useFetch from '../../hooks/useFetch';
-import route from '../../route';
-import { getDateString } from '../../utils/dateUtil';
-import styles from './Home.module.css';
-
-const { Paragraph } = Typography;
-
-function ListFooterItem({ icon, text, size, className, onClick }) {
-  return (
-    <Space size={size} className={className} onClick={onClick}>
-      {icon && React.createElement(icon)}
-      {text}
-    </Space>
-  );
-}
 
 function Home() {
-  const navigate = useNavigate();
   const { data, loading, error } = useFetch(getAllBlogs);
-
   return (
     error ?
       <Error status={error.status} message={error.message} /> :
-      <List
-        itemLayout="vertical"
-        size="large"
-        pagination={{
-          position: 'bottom',
-          align: 'center'
-        }}
-        loading={loading}
-        dataSource={loading ? [] : data}
-        renderItem={(item, index) => {
-          let previewText = item.previewText;
-          if (previewText && (previewText.slice(-1) !== '.')) {
-            previewText += ' ...';
-          }
-          const createdAt = getDateString(item.createdAt);
-
-          return (
-            <List.Item
-              key={index}
-            >
-              <List.Item.Meta
-                title={<a className={styles.listItemTitle}>{item.title || 'Untitled'}</a>}
-              />
-              {previewText && <Paragraph className={styles.listItemContent}>{previewText}</Paragraph>}
-              <Flex justify="space-between" className={styles.listItemFooter}>
-                <Space size="middle">
-                  <ListFooterItem text={createdAt} />
-                  {/*<ListFooterItem icon={LikeOutlined} text={item.likes} />*/}
-                  {/*<ListFooterItem icon={MessageOutlined} text={item.comments} />*/}
-                </Space>
-                <Space split={<Divider type="vertical" />} size={4}>
-                  <ListFooterItem
-                    className={styles.clickable}
-                    icon={EditOutlined}
-                    text="Edit"
-                    size={6}
-                    onClick={() => {
-                      navigate(`${route.edit}/${item._id}`);
-                    }}
-                  />
-                  <ListFooterItem className={styles.clickable} icon={DeleteOutlined} text="Delete" size={6} />
-                </Space>
-              </Flex>
-            </List.Item>
-          );
-        }}
-      />
+      <BlogList data={data} loading={loading} />
   );
 }
 
