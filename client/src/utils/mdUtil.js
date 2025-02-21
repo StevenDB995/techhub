fa3820db0866268e56789ad8843ab7ba8b91d5f7
StevenDB <xieyijie98@gmail.com>
2025-02-21 22:16:08 +1100

    fix[blog]: do not parse image as the preview text; further secure the process with dompurify

--- client/src/utils/mdUtil.js
+++ client/src/utils/mdUtil.js
@@ -1,6 +1,8 @@
+import DOMPurify from 'dompurify';
+
 export const extractMetadata = (html) => {
   const parser = new DOMParser();
-  const doc = parser.parseFromString(html, 'text/html');
+  const doc = parser.parseFromString(DOMPurify.sanitize(html), 'text/html');
 
   // Select the first <h1> and first <p> element that is not from toc
   const h1 = doc.querySelector('h1');
@@ -14,12 +16,25 @@ export const extractMetadata = (html) => {
 
   let maxLength = 280;
   if (p) {
-    const pHtml = p.innerHTML;
     // Only retrieve the content before the first line break of the first <p> element for preview text
-    const pText = pHtml.split('<br>')[0].trim();
+    const pHtml = p.innerHTML;
+    const pLines = pHtml.split('<br>');
+    let pText = '';
+
+    for (let pLine of pLines) {
+      const pLineDom = document.createElement('p');
+      pLineDom.innerHTML = pLine.trim();
+      const pLineText = pLineDom.innerText;
+      if (pLineText) {
+        pText = pLineText;
+        break;
+      }
+    }
+
     while (maxLength < pText.length && pText[maxLength] !== ' ') {
       maxLength++;
     }
+
     previewText = pText.slice(0, maxLength);
   }
 
@@ -28,7 +43,7 @@ export const extractMetadata = (html) => {
 
 export const extractImageLinks = (html) => {
   const parser = new DOMParser();
-  const doc = parser.parseFromString(html, 'text/html');
+  const doc = parser.parseFromString(DOMPurify.sanitize(html), 'text/html');
   const imgList = doc.querySelectorAll('img');
   const links = [];
   for (let img of imgList) {
